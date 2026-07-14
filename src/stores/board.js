7bd280ee87fd1eb0b510824b6465311adab94623
ik <toqurdk27@gmail.com>
2026-07-14 16:50:23 +0900

    Refactor(Board): board stores 로직 정리

--- src/stores/board.js
+++ src/stores/board.js
@@ -1,8 +1,20 @@
 import { defineStore } from "pinia"
 
-import { getPosts, createPost, getPostById, updatePost, deletePost, checkPassword } from "@/servives/boardApi"
+
+import {
+    getPosts,
+    createPost,
+    getPostById,
+    updatePost,
+    deletePost,
+    checkPassword
+
+} from "@/services/boardApi"
+
+
 
 export const useBoardStore = defineStore("board", {
+
 
     state:()=>({
 
@@ -10,62 +22,153 @@ export const useBoardStore = defineStore("board", {
 
     }),
 
+
+
     actions:{
+
+
+        // 게시글 전체 조회
 
         async loadPosts(){
 
-            this.posts = await getPosts()
+
+            const data = await getPosts()
+
+
+            this.posts = data
+
 
         },
 
-        async addPost(post) {
-            
-            const newPost = await createPost(post)
+
+
+        // 게시글 작성
+
+        async addPost(post){
+
+
+            const newPost =
+                await createPost(post)
+
+
+
             this.posts.unshift(newPost)
+
+
         },
+
+
+
+        // 게시글 상세 조회
 
         async findPost(id){
 
+
             return await getPostById(id)
+
+
         },
 
-        async updatePost(id, form) {
 
-            const index = this.posts.findIndex(
-                post => post.id === Number(id)
-            )
 
-            if(index === -1) return false
+        // 게시글 수정
 
-            this.posts[index] = {
-                ...this.posts[index],
-                title: form.title,
-                content: form.content
+        async updatePost(id,form){
+
+
+            const updatedPost =
+                await updatePost(
+                    id,
+                    form
+                )
+
+
+
+            if(!updatedPost){
+
+                return false
+
             }
 
+
+
+            const index =
+                this.posts.findIndex(
+                    post =>
+                    post.id === Number(id)
+                )
+
+
+
+            if(index !== -1){
+
+                this.posts[index] =
+                    updatedPost
+
+            }
+
+
+
             return true
+
+
         },
+
+
+
+        // 게시글 삭제
 
         async removePost(id){
 
-            const success = await deletePost(id)
 
-            if(!success) return false
+            const success =
+                await deletePost(id)
 
-            this.posts = this.posts.filter(
-                post => post.id !== Number(id)
-            )
+
+
+            if(!success){
+
+                return false
+
+            }
+
+
+
+            this.posts =
+                this.posts.filter(
+
+                    post =>
+                    post.id !== Number(id)
+
+                )
+
+
 
             return true
 
+
         },
+
+
+
+        // 비밀번호 확인
 
         async checkPostPassword(id,password){
 
-            return await checkPassword(id,password)
 
-        }        
+            return await checkPassword(
+
+                id,
+
+                password
+
+            )
+
+
+        }
+
 
     }
+
 
 })
